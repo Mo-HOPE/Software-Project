72b86984d7155c7e3cde59f5764b89d85bab5b07
MeRnA <m.salah2284@nu.edu.eg>
2024-12-20 18:25:13 +0200

    creating the ui of the home screen

--- frontend/lib/views/home_view.dart
+++ frontend/lib/views/home_view.dart
@@ -1,14 +1,79 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/widgets/product_card.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
   const HomeView({super.key});
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Working on that",
-            style: Theme.of(context).textTheme.headlineMedium),
+      appBar: AppBar(
+        title: const Text('OutfitOn'),
+        titleTextStyle: GoogleFonts.playfairDisplay(
+          color: Colors.white,
+          fontSize: 22.0,
+          fontWeight: FontWeight.w600,
+        ),
+        backgroundColor: Colors.purple,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: GridView.builder(
+          physics: const BouncingScrollPhysics(),
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            crossAxisSpacing: 8.0,
+            mainAxisSpacing: 8.0,
+            childAspectRatio: 3 / 4,
+          ),
+          itemCount: 10,
+          itemBuilder: (context, index) {
+            return ProductCard(
+              title: 'Product $index',
+              price: '\$${(index + 1) * 10}',
+              imageUrl:
+                  'https://i8.amplience.net/i/jpl/jd_729932_a?qlt=92&w=950&h=1212&v=1&fmt=auto',
+            );
+          },
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart),
+            label: 'Cart',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: 'Wishlist',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.purple,
+        onTap: _onItemTapped,
       ),
     );
   }
