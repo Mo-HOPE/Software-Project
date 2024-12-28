d6fd7f72f6b0acef95ff1201a995c28bbcff6e7e
Mohamed Nasser <m.nasser2209@nu.edu.eg>
2024-12-28 17:49:21 +0200

    reslove issues

---
+++ frontend/lib/global.dart
@@ -0,0 +1,11 @@
+import 'package:frontend/models/customer_model.dart';
+
+CustomerModel globalCustomer = CustomerModel(
+  name: '',
+  email: '',
+  password: '',
+  address: '',
+  phone: '',
+  cartProducts: [],
+  whishListProducts: [],
+);
