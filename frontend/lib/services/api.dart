528133a065bd502979f85cafa6f421539c988497
Mohamed Nasser <m.nasser2209@nu.edu.eg>
2024-12-29 03:44:34 +0200

    linking between home screen components

--- frontend/lib/services/api.dart
+++ frontend/lib/services/api.dart
@@ -1,12 +1,13 @@
 import 'package:dio/dio.dart';
+import 'package:frontend/models/product_model.dart'; // Adjust the import based on your project structure
 
 class ApiService {
   final Dio _dio = Dio(
     BaseOptions(
       baseUrl:
-          'https://ouitfiton-h3akced7ffevhfcs.canadacentral-01.azurewebsites.net/api/',
-      connectTimeout: Duration(seconds: 30),
-      receiveTimeout: Duration(seconds: 30),
+          'https://outfitonv2-hqdnefb7hfdtg2gm.canadacentral-01.azurewebsites.net/api/',
+      connectTimeout: const Duration(seconds: 30),
+      receiveTimeout: const Duration(seconds: 30),
     ),
   );
 
@@ -102,6 +103,22 @@ class ApiService {
       return response;
     } on DioException catch (e) {
       rethrow;
+    }
+  }
+
+  // New getProducts function
+  Future<List<ProductModel>> getProducts(String category) async {
+    try {
+      final response = await _dio.get('get-products/$category/');
+      List<dynamic> data = response.data;
+
+      // Map API response to a list of ProductModel instances
+      return data.map((product) => ProductModel.fromMap(product)).toList();
+    } on DioException catch (e) {
+      // Handle specific Dio errors here if needed
+      throw Exception('Failed to fetch products: ${e.message}');
+    } catch (e) {
+      throw Exception('Unexpected error: $e');
     }
   }
 }
